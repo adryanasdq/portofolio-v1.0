d5f64ba2cc45eb673e60984352e8dd0cf0ce436c
adryanashidiq <adryan.ashidiq@navcore.com>
2025-05-25 00:21:10 +0700

    Migrate to typescript, use latest version of Nextjs

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "rgb(var(--background))",
+        foreground: "rgb(var(--foreground))",
+        muted: "rgb(var(--muted))",
+        accent: "rgb(var(--accent))",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
